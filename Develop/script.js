7c7fc44e687d0b08798633fbd54a660093c2c5cd
Aserrano7 <serranoandres022@gmail.com>
2023-11-06 14:20:23 -0800

    added time keeper function

--- Develop/script.js
+++ Develop/script.js
@@ -41,3 +41,36 @@ $(".saveBtn").on("click", function () {
   //saves those values in local storage
   localStorage.setItem(time, text);
 });
+
+
+// this function will get the present time and format it to be 0-24 hours. It then goes through each time block getting each specific time from that block and compares that value to the value of the current time 
+function timerKeeper(){
+  var presentTime = dayjs().format('H');
+
+$(".time-block").each(function () {
+  var timeBox = parseInt($(this).attr("id").split("hour-")[1]);
+
+  //checks if time is equal to each other than add the class present and remove all other classes
+ if (timeBox == presentTime) {
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).addClass("present");
+
+}
+//checks if the time is less than present time and adds the past class to it
+  else if (timeBox < presentTime) {
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+    $(this).addClass("past");
+}
+ 
+  //add the future class to it 
+  else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+
+  }
+});
+}
+timerKeeper();
